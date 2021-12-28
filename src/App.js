9c9171a785c9577e7ee8b501b7daad8d4dccf6e5
Ionut Misa <ionut.misa21@gmail.com>
2021-12-28 21:48:11 +0100

    creting the TodoForm and adding it into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.scss";
+import TodoForm from "./components/TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(todo) {
+    setTodo([todo, ...todos]);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>React ToDo App</p>
+        <TodoForm addTodo={addTodo} />
       </header>
     </div>
   );
